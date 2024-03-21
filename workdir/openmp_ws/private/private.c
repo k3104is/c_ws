e8d8f404e3a39ac4df12a660a73f9e0eb5c6ba8d
k3104is <k.3104is@gmail.com>
2024-03-21 19:39:51 +0900

    modify private to post output

--- workdir/openmp_ws/private/private.c
+++ workdir/openmp_ws/private/private.c
@@ -5,14 +5,26 @@ int main()
   int i, j;
   int a[9] = {1,2,3,4,5,6,7,8,9};
   int b[9] = {1,2,3,4,5,6,7,8,9};
+  int c[9][9] = {0};
 
+#ifdef NG
 #pragma omp parallel for
-// #pragma omp parallel for private(j)
+#else
+#pragma omp parallel for private(j)
+#endif
   for(i = 0; i < 9; i++)
   {
     for(j = 0; j < 9; j++)
     {
-      printf("%d * %d = %d\n", a[i], b[j], (a[i] * b[j]));
+      c[i][j] = a[i] * b[j];
+    }
+  }
+
+  for(i = 0; i < 9; i++)
+  {
+    for(j = 0; j < 9; j++)
+    {
+      printf("%d * %d = %d\n", a[i], b[j], c[i][j]);
     }
   }
   return 0;
